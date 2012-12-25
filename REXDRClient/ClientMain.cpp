59a809a0998ac7c04e84a0846d0e21924d197b1c
Hong SeongBong <stallon72@naver.com>
2012-12-25 23:46:22 +0900

    start to add REXDRClient codes

--- REXDRClient/ClientMain.cpp
+++ REXDRClient/ClientMain.cpp
@@ -2,10 +2,48 @@
 //
 
 #include "stdafx.h"
+#include <REXDR/REXDRConnectorAPI.h>
+#include "Log4XWrapper.h"
+
+#define SERVER_IP		"10.64.50.209"
+#define SERVER_PORT		13403
+
+// Definitions for Log4XWrapper
+#define LOG4X_NAMESPACE		_T("REXDRClientServerLog4X")
+#define LOG4X_CONFIGFILE	_T("REXDRClientServerLog4X.xml")
+#ifdef _DEBUG
+#define LOGGER_NAME			_T("DebugLogger")
+#else
+#define	LOGGER_NAME			_T("ReleaseLogger")
+#endif
+
+
+
+// Callback Function - Default Response Handler
+void __stdcall OnResponse( REXDR::Connector::ErrorCode errorCode, const REXDR::Response& resp, void* context)
+{
+
+
+}
+
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	// define global logger
+	Log4XWrapper gLogger(LOG4X_NAMESPACE, LOG4X_CONFIGFILE, LOGGER_NAME);
+
+	if ( !gLogger.IsLoggerEnabled() )
+	{
+		printf(">>> [REXDRServer: main.cpp] Log4X Initialization Failed. \n");
+		return -1;
+	}
+
+	REXDR::Connector::Handle hConnector = REXDR::Connector::CreateHandle(REXDR::Connector::TRANSPORT_TCP, SERVER_IP, SERVER_PORT, 
+
+
+
+
 	return 0;
 }
 
